8e2d10d34dda4743ee9c391b5cea8017bf84eedb
Mark Nottingham <mnot@mnot.net>
2019-09-25 12:17:30 +1000

    simplify update script

--- update.sh
+++ update.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
+
+# Check to see if repo_data has changed.
+git status --short repo_data.json | grep -s "M" || exit 0
+
 # setup
 git config user.email mnot@mnot.net
 git config user.name mnot-bot
 git remote set-url --push origin https://mnot:$GITHUB_TOKEN@github.com/mnot/ietf-repo-data
-
-# Update repo_data.json
-python3 repo_spider.py > repo_data.json
-
-# Check to see if repo_data has changed.
-git status --short repo_data.json | grep -s "M" || exit 0
 
 # Push the changes.
 git add repo_data.json
