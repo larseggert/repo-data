6d7e436d6d130c11f0a218e78d5c0ea8a0715e69
Mark Nottingham <mnot@mnot.net>
2019-09-25 12:06:52 +1000

    set up github for update

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# setup
+git config user.email mnot@mnot.net
+git config user.name mnot-bot
+git remote set-url --push origin https://mnot:$GITHUB_TOKEN@github.com/mnot/ietf-repo-data
+
 # Check to see if repo_data has changed.
 git status --short repo_data.json | grep -s "M" || exit 0
 
