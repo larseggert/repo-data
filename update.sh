76238cf73b8ddc770ac3d6a14ebccf2e0cbf5e70
Mark Nottingham <mnot@mnot.net>
2019-09-25 12:07:58 +1000

    move away from makefile

--- update.sh
+++ update.sh
@@ -5,6 +5,9 @@ git config user.email mnot@mnot.net
 git config user.name mnot-bot
 git remote set-url --push origin https://mnot:$GITHUB_TOKEN@github.com/mnot/ietf-repo-data
 
+# Update repo_data.json
+python3 repo_spider.py > repo_data.json
+
 # Check to see if repo_data has changed.
 git status --short repo_data.json | grep -s "M" || exit 0
 
