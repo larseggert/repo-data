bd2bab36343c891e485b35378b57be8cd912e49b
Mark Nottingham <mnot@mnot.net>
2021-02-26 08:32:35 +1100

    master -> main

--- update.sh
+++ update.sh
@@ -8,9 +8,9 @@ git status --short repo_data.json | grep -s "M" || exit 0
 git config user.email mnot@mnot.net
 git config user.name mnot-bot
 git remote set-url --push origin https://mnot:$GITHUB_TOKEN@github.com/ietf-github-services/repo-data
-git checkout -B master origin/master
+git checkout -B main origin/master
 
 # Push the changes.
 git add repo_data.json
 git commit -m "update repo_data.json"
-git push origin master
+git push origin main
